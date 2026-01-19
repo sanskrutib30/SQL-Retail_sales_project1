7a99d6f93674ffa256a8240fdcd842b5917fe0a1
Sanskruti bhosale <sanskrutibhosale300@gmail.com>
2026-01-19 17:40:59 +0530

    Update SQL queries and project findings

--- SQl project1.sql
+++ SQl project1.sql
@@ -75,52 +75,70 @@ select count(distinct(customer_id)) as Total_customers from retail_sales;
 -- Data Analysis & Business Key Problems & Answers
 
 -- My Analysis & Findings
--- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+1. **Write a SQL query to retrieve all columns for sales made on '2022-11-05'**: 
+ '''sql 
 select * from retail_sales
  where sale_date = '2022-11-05';
+'''
  
-# Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+2. **Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022**: 
+ '''sql 
 select * from retail_sales 
 where category = 'Clothing' 
 and quantity >= 4
 and sale_date between '2022-11-01' and '2022-11-30';
+ '''
 
--- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
-select category, sum(total_sale)as Total_sales, count(*) as total_orders from retail_sales
+3. **Write a SQL query to calculate the total sales (total_sale) for each category**: 
+ '''sql 
+select category, sum(total_sale)as Total_sales, 
+count(*) as total_orders from retail_sales
 group by 1;
+'''
 
--- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+4. **Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category**: 
+ '''sql 
 select round(avg(age)) as Avg_age from retail_sales
 where category ='Beauty';
 select * from retail_sales;
-
--- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
-select * from retail_sales 
+'''
+ 
+5. **Write a SQL query to find all transactions where the total_sale is greater than 1000**: 
+ '''sql 
+select * from retail_sales
 where total_sale > 1000;
+'''
 
--- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+6. **Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category**: 
+ '''sql 
 select category,gender, count(*) as Total_No from retail_sales
 group by 1,2
 order by 1;
-
--- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+'''
+ 
+7. **Write a SQL query to calculate the average sale for each month. Find out best selling month in each year**: 
+ '''sql 
 select year(sale_date) as Year, month(sale_date) as Month, sum(total_sale) as Total_sale from retail_sales
 group by 1, 2 
 order by 1,2 desc;
--- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+ '''
+ 
+8. **Write a SQL query to find the top 5 customers based on the highest total sales**: 
+ '''sql 
 select  customer_id, sum(total_sale)from retail_sales 
 group by 1
 order by 2 desc
 limit 5;
-
--- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+''' 
+ 
+9. **Write a SQL query to find the number of unique customers who purchased items from each category )**: 
+ '''sql
 select  category ,count(distinct(customer_id)) as total_no from retail_sales
 group by 1;
+'''
 
-
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
-select * from retail_sales;
-
+10. **Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)**: 
+ '''sql
 SELECT *,
 CASE
     WHEN HOUR(sale_time) < 12 THEN 'Morning'
@@ -128,5 +146,22 @@ CASE
     ELSE 'Evening'
 END AS shift
 FROM retail_sales;
+'''
+**End of project**
+ 
+## Findings
 
-# End of project
+- **Customer Demographics**: The dataset includes customers from various age groups, with sales distributed across different categories such as Clothing and Beauty.
+- **High-Value Transactions**: Several transactions had a total sale amount greater than 1000, indicating premium purchases.
+- **Sales Trends**: Monthly analysis shows variations in sales, helping identify peak seasons.
+- **Customer Insights**: The analysis identifies the top-spending customers and the most popular product categories.
+
+## Reports
+
+- **Sales Summary**: A detailed report summarizing total sales, customer demographics, and category performance.
+- **Trend Analysis**: Insights into sales trends across different months and shifts.
+- **Customer Insights**: Reports on top customers and unique customer counts per category.
+
+## Conclusion
+- **This project serves as a comphrensive introduction to SQL for data analysts, covering database setup, data cleaning, exploratory data analysis, and bsuiness driven SQL queries. 
+- **The findings from this project can help drive business decisions by understanding sales patterns, customer behaviour, and product performance.
